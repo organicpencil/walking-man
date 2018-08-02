4ab1c602d2e5b4cbac0c24e8dde494043c062b7a
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2018-08-02 10:24:16 -0500

    Walker improvements

--- project/contestants/walker/walker.gd
+++ project/contestants/walker/walker.gd
@@ -1,6 +1,6 @@
 extends KinematicBody
 
-var speed = 1.0
+var speed = 0.4
 var mouselook = Vector2()
 var controls = {"forward": false, "back": false, "left": false, "right": false, "primary": false, "secondary": false}
 var input_prefix = ""
@@ -9,9 +9,16 @@ func _ready():
 	$AnimationPlayer.play("walker-idle-loop", 0.1)
 
 func _physics_process(delta):
-	rotate_y(-mouselook.x)
+	var look = 0.0
+	if controls['left']:
+		look += 0.01
+	if controls['right']:
+		look -= 0.01
+
+	rotate_y(look)
+
+	$Camera.rotation.y = clamp($Camera.rotation.y - mouselook.x, -1.57, 1.57)
 	$Camera.rotation.x = clamp($Camera.rotation.x - mouselook.y, -1.57, 1.57)
-	#$Camera.rotate_x(-mouselook.y)
 	mouselook = Vector2()
 
 	var move = Vector3()
@@ -24,6 +31,7 @@ func _physics_process(delta):
 	if controls['forward']:
 		move += -basis[2]
 
+	"""
 	if controls['back']:
 		move += basis[2]
 
@@ -32,9 +40,10 @@ func _physics_process(delta):
 
 	if controls['right']:
 		move += basis[0]
+	"""
 
-		move.y = 0
-		move = move.normalized()
+	move.y = 0
+	move = move.normalized()
 
 	if anim == "walker-walk-loop" and pos > 0.2 and pos < 0.8:
 		move_and_slide(move * speed + Vector3(0, -5, 0), Vector3(0, 1, 0))
